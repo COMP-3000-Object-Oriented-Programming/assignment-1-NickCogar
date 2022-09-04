ce8f4a7de61bed226ac9b0d83e3209b078d2686f
NickCogar <nbc0011@auburn.edu>
2022-09-04 10:49:03 -0500

    Finished assignment

--- assignment_1/q4.cpp
+++ assignment_1/q4.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main (){
 
-    
+    int time;
+    int distance;
+
+    cout << "Enter a time in seconds. "<< endl;
+    cin >> time;
+
+    distance = 0.5 * 32 * (time * time);
+    cout << "An object in freefall for " << time << " seconds will fall " << distance << " feet." << endl;
+
+    return 0;
 }
